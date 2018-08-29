8e36ecb47b5a5b32b92a6c8e5d33f1be6be3f47e
cmonkey03 <russellpbrown@gmail.com>
2018-08-29 10:03:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function printBadges(arr) {
+  for (i = 0; i < arr.length; i++) {
+    console.log(`Welcome ${arr[i]}! You are employee #${i+1}.`);
+  }
+  return arr
+}
+
+function tailsNeverFails() {
+  function coinToss() {
+    return Math.random() >= .5;
+  }
+  let i = 0;
+  while (coinToss()) {
+    i++
+  }
+  return `You got ${i} tails in a row!`;
+}
